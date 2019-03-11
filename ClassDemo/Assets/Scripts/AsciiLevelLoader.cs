0090390d09c401b7e926e8b455e98b10270e9757
177sma <howerds4my@gmail.com>
2019-03-11 17:24:36 -0400

    Initializing Homework5

---
+++ ClassDemo/Assets/Scripts/AsciiLevelLoader.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class AsciiLevelLoader : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        string filePath = Application.dataPath + "/level0.txt";
+
+        if (!File.Exists(filePath))
+        {
+            File.WriteAllText(filePath, "X");
+        }
+
+        string[] inputLines = File.ReadAllLines(filePath);
+
+        for (int y = 0; y < inputLines.Length; y++)
+        {
+            string line = inputLines[y];
+    
+            for (int x = 0; x < line.Length; x++)
+            {
+/*                if (line[x] == 'X')
+                {
+                    //make a wall
+                    GameObject newWall = Instantiate(Resources.Load("Prefabs/Wall")) as GameObject;
+                    newWall.transform.position = new Vector2(x - line.Length/2f, inputLines.Length/2f - y);
+                }
+                else if (line[x] == 'P')
+                {
+                    //make a wall
+                    GameObject newPlayer = Instantiate(Resources.Load("Prefabs/Player")) as GameObject;
+                    newPlayer.transform.position = new Vector2(x - line.Length/2f, inputLines.Length/2f - y);
+                }
+                else if (line[x] == 'G')
+                {
+                    //make a wall
+                    GameObject newGold = Instantiate(Resources.Load("Prefabs/Gold")) as GameObject;
+                    newGold.transform.position = new Vector2(x - line.Length/2f, inputLines.Length/2f - y);
+                }
+                else if (line[x] == 'T')
+                {
+                    //make a wall
+                    GameObject newTrap = Instantiate(Resources.Load("Prefabs/Trap")) as GameObject;
+                    newTrap.transform.position = new Vector2(x - line.Length/2f, inputLines.Length/2f - y);
+                }*/
+
+                GameObject tile = null;
+
+                switch (line[x])
+                {
+                    case 'X':
+                        tile = Instantiate(Resources.Load("Prefabs/Wall")) as GameObject;
+                        break;
+                    case 'P':
+                        tile = Instantiate(Resources.Load("Prefabs/Player")) as GameObject;
+                        break;
+                    case 'G':
+                        tile = Instantiate(Resources.Load("Prefabs/Gold")) as GameObject;
+                        break;
+                    case 'T':
+                        tile = Instantiate(Resources.Load("Prefabs/Trap")) as GameObject;
+                        break;
+                    default:
+                        tile = null;
+                        break;
+                }
+
+                if (tile != null)
+                {
+                    tile.transform.position = new Vector2(x - line.Length/2f, inputLines.Length/2f - y);
+                }
+            }        
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Homework5/Assets/Script/PlayerControl.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.XR.WSA.Input;
+
+public class PlayerControl : MonoBehaviour
+{
+    [Header("Control")]
+    public KeyCode Left = KeyCode.A;
+    public KeyCode Right = KeyCode.D;
+    public KeyCode Up = KeyCode.W;
+    public KeyCode Down = KeyCode.S;
+    public KeyCode Attack = KeyCode.J;
+    public KeyCode Jump = KeyCode.Space;
+    
+    [Header("Attribute")]
+    public float MovingSpeed;
+    public float JumpingSpeed;
+    public int ComboFrameRange;
+
+    private Vector2 SpeedH = new Vector2(0, 0);
+    private Vector2 SpeedV = new Vector2(0, 0);
+    private Rigidbody2D rb;
+    private bool landed;
+
+    [Header("Game Stat")]
+    public Animator animator;
+    public bool LookRight = true;
+    public int AttackStage;
+    public int ComboBar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        SpeedH.Set(MovingSpeed, 0);
+        SpeedV.Set(0, JumpingSpeed);
+    }
+    
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        print("Landed");
+        landed = true;
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        print("Flowing");
+        landed = false;
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {        
+        //MoveMent
+        if (Input.GetKeyDown(Left))
+        {
+            rb.velocity -= SpeedH;
+        }
+        
+        if (Input.GetKeyUp(Left))
+        {    
+            rb.velocity += SpeedH;
+        }
+
+        if (Input.GetKeyDown(Right))
+        {
+            rb.velocity += SpeedH;
+        }
+
+        if (Input.GetKeyUp(Right))
+        {
+            rb.velocity -= SpeedH;
+        }
+        
+        if (landed && Input.GetKeyDown(Jump))
+        {
+            rb.velocity += SpeedV;
+        }
+        
+        //Flip
+        if ((rb.velocity.x > 0 && LookRight) || (rb.velocity.x < 0 && !LookRight))
+        {
+            LookRight = !LookRight;
+            Vector3 CharScale = transform.localScale;
+            CharScale.x *= -1;
+            transform.localScale = CharScale;
+        }
+        
+        //Attack        
+        if (ComboBar > 0)
+        {
+            ComboBar--;
+            if (ComboBar == 0)
+            {
+                AttackStage = 0;
+                animator.SetInteger("AttackStage", AttackStage);
+            }
+        }
+        
+        if (Input.GetKeyDown(Attack) && AttackStage == 2 && ComboBar < 30)
+        {
+            AttackStage++;
+            animator.SetInteger("AttackStage", AttackStage);
+            ComboBar += ComboFrameRange;
+        }
+        
+        if (Input.GetKeyDown(Attack) && AttackStage == 1)
+        {
+            AttackStage++;
+            animator.SetInteger("AttackStage", AttackStage);
+            ComboBar += ComboFrameRange;
+        }        
+        
+        if (Input.GetKeyDown(Attack) && AttackStage == 0)
+        {
+            AttackStage++;
+            animator.SetInteger("AttackStage", AttackStage);
+            ComboBar += ComboFrameRange;
+        }
+    }
+
+
+}
